891ca1ee213293519c5e458564cdb7f60fa27555
longndt <55429399+longndt@users.noreply.github.com>
2022-02-12 09:47:29 +0700

    web

---
+++ demoweb3/src/Form/MovieType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use App\Entity\Movie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('image')
+            ->add('year')
+            ->add('genre', EntityType::class,
+            [
+                'label' => 'Movie Genres',
+                'class' => Genre::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true
+                /* Note: expanded = true => checkbox
+                        expanded = false => dropdown list */
+            ])
+            ->add('Save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
